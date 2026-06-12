ea88319cf123f8874236973569010c5568653c40
Danish <dan71ish@gmail.com>
2026-06-12 21:09:01 +0530

    initial commit

---
+++ components/login.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+export default function Login() {
+    return (
+        <main className="flex items-center w-full h-screen justify-center px-4">
+            <div className="w-full max-w-[18rem]">
+                <h3 className="text-center mb-5 text-lg font-semibold">What's your email address?</h3>
+                <Input placeholder="Enter your email address..." className="py-5.5 w-full" />
+                <div className="flex flex-col mt-4 gap-2">
+                    <Button className="py-6 rounded-full" variant={"secondary"}>Continue with email</Button>
+                    <Button variant={'link'}>Back to login</Button>
+                </div>
+            </div>
+        </main>
+    )
+}
