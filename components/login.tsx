8376d3813429ae955e47e2889959d4b71f78a0cb
Danish <dan71ish@gmail.com>
2026-06-12 21:40:48 +0530

    fix

--- components/login.tsx
+++ components/login.tsx
@@ -3,13 +3,13 @@ import { Input } from "./ui/input";
 
 export default function Login() {
     return (
-        <main className="flex items-center w-full h-dvh justify-center px-4">
-            <div className="w-full max-w-xs">
-                <h3 className="text-center mb-5 text-xl font-semibold">What's your email address?</h3>
-                <Input placeholder="Enter your email address..." className="py-5.5 w-full" />
-                <div className="flex flex-col mt-4 gap-2">
-                    <Button className="py-6 rounded-full" variant={"secondary"}>Continue with email</Button>
-                    <Button variant={'link'}>Back to login</Button>
+        <main className="flex items-center w-full h-dvh justify-center px-2">
+            <div className="w-full max-w-sm md:max-w-xs">
+                <h3 className="text-center mb-6 text-2xl md:text-xl font-semibold">What's your email address?</h3>
+                <Input placeholder="Enter your email address..." className="py-7 md:py-5 w-full" />
+                <div className="flex flex-col mt-4 gap-3">
+                    <Button className="py-6.5 md:py-5 text-md md:text-xs rounded-full" variant={"secondary"}>Continue with email</Button>
+                    <Button variant={'link'} className="text-sm md:text-xs">Back to login</Button>
                 </div>
             </div>
         </main>
