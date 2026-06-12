807550b833b0b85f0b4147d21fe4363f571ebecd
Danish <dan71ish@gmail.com>
2026-06-12 21:17:14 +0530

    fk

--- components/login.tsx
+++ components/login.tsx
@@ -5,7 +5,7 @@ export default function Login() {
     return (
         <main className="flex items-center w-full h-screen justify-center px-4">
             <div className="w-full max-w-xl">
-                <h3 className="text-center mb-5 text-lg font-semibold">What's your email address?</h3>
+                <h3 className="text-center mb-5 text-xl font-semibold">What's your email address?</h3>
                 <Input placeholder="Enter your email address..." className="py-5.5 w-full" />
                 <div className="flex flex-col mt-4 gap-2">
                     <Button className="py-6 rounded-full" variant={"secondary"}>Continue with email</Button>
